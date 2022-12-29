a24bb1a864561ceaa9e803b43cbc934592b24b19
NicoAquino67 <nicoanriquez23@gmail.com>
2022-12-29 09:10:42 +0100

    tipado de js

---
+++ Tipado-JS.js
@@ -0,0 +1,27 @@
+//tipos primitivos
+//numbers
+var number = 1;
+var number2 = -1;
+var number3 = 1.4;
+// strings
+var string = "hola mundo"
+var string2 = 'hola mundo'
+var string3 = `hola mundo`
+//bools
+var bool1 = true
+var bool0 = false
+
+//null and undefined
+var nul = null
+var undef = undefined
+
+// null, undefined, false, 0 ==> todas son false en estructuras de control.
+if (nul){
+    console.log("control")
+}else if (undef){
+    console.log("tol mundo enpatilla enla dicoteca")
+}else if (bool0){
+    console.log("tol mundo enpatilla enla dicoteca")
+}else{
+    console.log("tol mundo enpatilla enla dicoteca")
+}
